bac048227bffde934a282d71333617552113b055
Felipe Oliveira <felipeoliveira@MacBook-Pro-de-Felipe.local>
2020-02-25 23:25:09 -0300

    projeto criado, e components criados

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native'
+import params from './src/params'
+import Field from './src/components/Field'
+
+export default class App extends Component {
+
+  render (){
+    return (
+      <View style={styles.container}>
+        <Text>
+          Tamanho da grade: 
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={3} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+      </View>
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  }
+});
---
+++ ios/Pods/Target Support Files/Pods-minesTests/Pods-minesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_minesTests : NSObject
+@end
+@implementation PodsDummy_Pods_minesTests
+@end
